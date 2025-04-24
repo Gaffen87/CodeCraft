60196119f3d71eb4b30328938a29d69f5c8060bb
Aske Lysgaard <askelysgaard@gmail.com>
2025-04-24 11:13:51 +0200

    til andreas

--- CodeCraftUI/app/features/exercises/layout.tsx
+++ CodeCraftUI/app/features/exercises/layout.tsx
@@ -1,6 +1,9 @@
 import { Outlet } from "react-router";
+import useExercise from "~/hooks/useExercise";
 
 export default function ExerciseLayout() {
+	const { exercise, loading } = useExercise({exerciseId: 3});
+
 	return (
 		<div>
 			<Outlet />
---
+++ CodeCraftUI/app/hooks/useExercise.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+export default function useExercise({ exerciseId }) {
+	const [exercise, setExercise] = useState();
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		const fetchExercise = async () => {
+			try {
+				const response = await fetch("https://localhost:7060/api/exercise/1", {
+					method: "GET",
+					headers: {
+						"Content-Type": "application/json",
+					},
+				});
+				if (!response.ok) {
+					throw new Error("Network response was not ok");
+				}
+				const data = await response.json();
+				setExercise(data);
+			} catch (error) {
+				console.error("Error fetching exercise:", error);
+			}
+		};
+
+		fetchExercise();
+	}, []);
+
+	return { exercise, loading, error };
+}
