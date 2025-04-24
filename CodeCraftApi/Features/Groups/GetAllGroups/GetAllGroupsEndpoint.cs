0029e6e8ffb11a3cb28d6f88af7b7f4680f3209e
Andreas B <Abarbesgaard@gmail.com>
2025-04-24 13:38:32 +0200

    groupfeature

---
+++ CodeCraftApi/Features/Groups/GetAllGroups/GetAllGroupsEndpoint.cs
@@ -0,0 +1,29 @@
+namespace CodeCraftApi.Features.Groups.GetAllGroups;
+
+using Database;
+
+internal sealed  class GetAllGroupsEndpoint(AppDbContext context) : EndpointWithoutRequest<GetAllGroupsResponse, Mapper>
+{
+    public override void Configure()
+    {
+        Get("/groups");
+        Description(x => x.WithName("Get all groups"));
+        AllowAnonymous();
+        Summary(new Summary());
+        
+    }
+
+    public override async Task HandleAsync( CancellationToken ct)
+    {
+        var groups = await Data.GetGroupsAsync(context);
+
+        if (groups == null)
+        {
+            await SendNotFoundAsync(ct);
+        }
+        else
+        {
+            await SendAsync(Map.FromEntity(groups), cancellation: ct);
+        }
+    }
+}
