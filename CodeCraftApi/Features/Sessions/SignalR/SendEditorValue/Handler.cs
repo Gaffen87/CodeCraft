610031eab25dbb0d141c7e3822f635c26bb33a9c
Aske Lysgaard <askelysgaard@gmail.com>
2025-05-06 09:33:01 +0200

    getsession

---
+++ CodeCraftApi/Features/Sessions/SignalR/SendEditorValue/Handler.cs
@@ -0,0 +1,13 @@
+﻿using CodeCraftApi.SignalR;
+using Microsoft.AspNetCore.SignalR;
+using SignalR.PepR;
+
+namespace CodeCraftApi.Features.Sessions.SignalR.SendEditorValue;
+
+internal sealed class SendEditorValueHandler(IHubContext<AppHub> hub) : HubMethodHandler<SendEditorValuePayload>
+{
+	protected override async Task HandleAsync(HubCallerContext context, SendEditorValuePayload payload)
+	{
+		await hub.Clients.Users(payload.UserId.ToString()).SendCoreAsync("ReceiveEditorValueMessage", [payload.EditorValue]);
+	}
+}
